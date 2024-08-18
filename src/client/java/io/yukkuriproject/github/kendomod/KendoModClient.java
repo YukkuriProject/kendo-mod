ef8fa35c3c00c80d98df71745d13fb443f19597b
ren <0806renkun@gmail.com>
2024-08-18 10:00:53 +0900

    initial commit

---
+++ src/client/java/io/yukkuriproject/github/kendomod/KendoModClient.java
@@ -0,0 +1,10 @@
+package io.yukkuriproject.github.kendomod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class KendoModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/io/yukkuriproject/github/kendomod/KendoMod.java
@@ -0,0 +1,10 @@
+package io.yukkuriproject.github.kendomod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class KendoMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
